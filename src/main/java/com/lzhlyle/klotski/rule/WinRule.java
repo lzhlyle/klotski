4a7f3f4942697c90f8a11c5b982af647ab5e3506
lzhlyle <lzhlyle@msn.cn>
2019-09-25 23:19:07 +0800

    Define victory conditions

--- src/main/java/com/lzhlyle/klotski/rule/WinRule.java
+++ src/main/java/com/lzhlyle/klotski/rule/WinRule.java
@@ -1,5 +1,14 @@
 package com.lzhlyle.klotski.rule;
 
+import com.lzhlyle.klotski.block.SquareBlock;
+import com.lzhlyle.klotski.board.BlockPlace;
+
+import java.util.List;
+
 public class WinRule extends Rule {
-    // DOING lzh WinRule
+    public boolean check(List<BlockPlace> blockPlaceList) {
+        // is the square block at (1, 0)
+        return blockPlaceList.stream().anyMatch(bp -> (bp.getBlock() instanceof SquareBlock)
+                && bp.getSouthwestCell().getLocation().equals(1, 0));
+    }
 }
